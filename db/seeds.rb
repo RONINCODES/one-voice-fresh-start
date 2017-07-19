a237ab7cea8c37d222e173412febf61378b1323f
mikesmall <michael.l.a.small@gmail.com>
2017-07-19 14:20:10 -0400

    'Faker' gem installed; basic User seeds created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+100.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.free_email,
+    role: 'student',
+    # password: 'password',
+    # password_confirmation: 'password'
+  )
+  puts user.first_name + " " + user.last_name + "(student) registered as a student."
+end
+
+20.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.free_email,
+    role: 'student',
+    # password: 'password',
+    # password_confirmation: 'password'
+  )
+  puts user.first_name + " " + user.last_name + " " + "got hired as an instructor."
+end

6590fd07a740728f0c28891b47df452310976d8c
mikesmall <michael.l.a.small@gmail.com>
2017-07-20 15:28:54 -0400

    Landing page and login/signup, semi-working

--- db/schema.rb
+++ db/schema.rb
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(version: 20170720154339) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
     t.string "password_digest"
   end
 

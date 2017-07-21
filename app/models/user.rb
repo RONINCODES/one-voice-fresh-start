4050343a258791c3352d7df7b28a496dbf48710e
Moodiex0 <moodiex0@gmail.com>
2017-07-21 07:00:44 -0400

    Change the path for the coment from in classsession show page and in classsession show method edit @users to limit the viewed userd to that each classsession, and add comments routs nested underc lcasssessions, and add function in checkin model and call it in the classsession controller to create a checkin on the click

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :class_rooms
+  has_many :check_ins
+  has_many :class_sessions, through: :check_ins
   has_many :comments
   has_many :surveys
-  has_many :check_ins
 
   validates :first_name, :last_name, :email, :role, :password, :password_confirmation, presence: true
 

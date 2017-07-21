f84cd4f6220ed2d172829cece2f64048519c93ae
mikesmall <michael.l.a.small@gmail.com>
2017-07-21 13:10:45 -0400

    'X'-style 'Close' buttons added to modals. Need beetter placement still

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,63 +22,35 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var modalSignUp = document.querySelector('#sign_up');
   var signIn = document.querySelector('.signin');
   var signUp = document.querySelector('.signup');
-
-  // var close = document.querySelector('.close');
+  var close = document.querySelector('.close');
   // var submit = document.querySelector('.submit');
 
   //-------------------------------------------------------------------------------------
-  // When the user presses the .signin button, fade in the modal window
-
+  // When user presses SIGN IN button, fade in the modal window
   function showModalWindowSignIn(click) {
     modalSignIn.style.display = 'block';
   };
   signIn.addEventListener('click', showModalWindowSignIn);
 
-  //-------------------------------------------------------------------------------------
-  // When the user presses the .signup button, fade in the modal window
-
+  // When user presses SIGN UP button, fade in the modal window
   function showModalWindowSignUp(click) {
     modalSignUp.style.display = 'block';
   };
   signUp.addEventListener('click', showModalWindowSignUp);
 
-
-  // When the user presses the .close button, fade out the modal window
-
-  function hideModalWindow(click) {
-    modal.style.display = 'none';
+  // When user presses CLOSE button, fade out the modal window
+  function hideModalWindowSignIn(click) {
+    modalSignIn.style.display = 'none';
   };
+  close.addEventListener('click', hideModalWindowSignIn);
 
-  // close.addEventListener('click', hideModalWindow);
 
-  //-------------------------------------------------------------------------------------
-  // When the user presses the .submit button, add an .error class to both input elements
-
-  function addError(click) {
-    var user = document.getElementById('user');
-    user.className = 'error';
-    var password = document.getElementById('pass');
-    password.className = 'error';
+  // When user presses CLOSE button, fade out the modal window
+  function hideModalWindowSignUp(click) {
+    modalSignUp.style.display = 'none';
   };
+  close.addEventListener('click', hideModalWindowSignUp);
 
-  // submit.addEventListener('click', addError);
-
-  //-------------------------------------------------------------------------------------
-  // When the user puts their cursor in one of the input fields, remove the .error class
-
-  var inputFields = document.querySelectorAll('input');
-
-  function removeError(click) {
-    var user = document.getElementById('user');
-    this.className = null;
-    var password = document.getElementById('pass');
-    this.className = null;
-  };
-
-  // inputFields[0].addEventListener('click', removeError);
-  // inputFields[1].addEventListener('click', removeError);
-
-  //-------------------------------------------------------------------------------------
 
 // End of 'DOMContentLoaded':
 });

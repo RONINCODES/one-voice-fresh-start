b31e4de251858ed8c3a9aa70b439820cb6e2096e
Ronen Annason <ronenannason83@gmail.com>
2017-07-26 11:25:05 -0400

    working on Survey Analytics Logic

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -10,7 +10,21 @@ class SurveysController < ApplicationController
 
 
   def show
+  @class_session = ClassSession.find(params[:class_session_id])
+  @helpfulness1and2 = Survey('helpfulness = ? or helpfulness = ?', '1', '2' )date.where('date(created_at)=?', @class_session.created_at).count
+  @helpfulness3 = Survey('helpfulness = ?', '3')date.where('date(created_at)=?', @class_session.created_at).count
+  @helpfulness4and5 = Survey('helpfulness = ? or helpfulness = ?', '4', '5' )date.where('date(created_at)=?', @class_session.created_at).count
 
+  @clarity1and2 = Survey('clarity = ? or clarity = ?', '1', '2' )date.where('date(created_at)=?', @class_session.created_at).count
+  @clarity3 = Survey('clarity = ?', '1')date.where('date(created_at)=?', @class_session.created_at).count
+  @clarity4and5 = Survey('clarity = ? or clarity = ?', '4', '5' )date.where('date(created_at)=?', @class_session.created_at).count
+
+  @pace1and2 = Survey('pace = ? or pace = ?', '1', '2' )date.where('date(created_at)=?', @class_session.created_at).count
+  @pace3 = Survey('pace = ?', '3')date.where('date(created_at)=?', @class_session.created_at).count
+  @pace4and5 = Survey('pace = ? or pace = ?', '4', '5' )date.where('date(created_at)=?', @class_session.created_at).count
+
+  @suggestions = Survey('suggestions.all').date.where('date(created_at)=?', @class_session.created_at)
+  @allsurveys = Survey.all.where('date(created_at)=?', @class_session.created_at).count
   end
 
   def create
